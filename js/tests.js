2024a944fc9dac6fa3e09f56d59bfb83456374c1
Demian <demyan.korotchenko@gmail.com>
2023-05-12 07:53:22 +0200

    20230511

--- js/tests.js
+++ js/tests.js
@@ -995,6 +995,37 @@ const atTheOldToad = {
   //atTheOldToad.updatePotionName({name: "F-16", price: 333}, {name: "B-52", price: 555});
   //console.log(atTheOldToad.getPotions());
   */
+
   
+  // Module 4_autocheck_4
+  // NEED to discuss with tutor hex: #f34336 is it possible to use FOR???
+  /*
+const pizzaPalace = {
+    pizzas: ["Ultracheese", "Smoked", "Four meats"],
+    order(pizzaName, onSuccess, onError) {
+      if (!this.pizzas.includes(pizzaName)) {
+        return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+      } 
+      return onSuccess(pizzaName);
+    },
+  };
+  // Change code above this line
   
+  // Callback for onSuccess
+  function makePizza(pizzaName) {
+    return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+  }
+  
+  // Callback for onError
+  function onOrderError(error) {
+    return `Error! ${error}`;
+  }
+  
+  // Method calls with callbacks
+  pizzaPalace.order("Smoked", makePizza, onOrderError);
+  pizzaPalace.order("Four meats", makePizza, onOrderError);
+  pizzaPalace.order("Big Mike", makePizza, onOrderError);
+  pizzaPalace.order("Vienna", makePizza, onOrderError);
+  */
+
   
