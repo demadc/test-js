ea2912b71613e832ca5e60f6752c857dafb2e2f4
Demian <demyan.korotchenko@gmail.com>
2023-07-03 14:57:56 +0200

    Update tests.js

--- js/tests.js
+++ js/tests.js
@@ -1429,3 +1429,47 @@ for (let num of numbers) {
 console.log(min);
 console.log(numbers);
 */
+
+//4.06.23
+
+// ЗАДАЧА 2
+// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА КІЛОГРАМ І КІЛЬКОСТІ КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+// html/
+
+
+const refs = {
+  price:document.querySelector('#price'),
+  quantity: document.querySelector('#quantity'),
+  text: document.querySelector('.text-right'),
+  form: document.querySelector('.form'),
+  amount: document.querySelector('.amount'),
+  btnCalc: document.querySelector('.btn'),
+}
+
+
+const valueQuantity = refs.quantity.value;
+const valuePrice = refs.price.value;
+
+function calc (e) {
+e.preventDefault();
+  const sum = valuePrice * valueQuantity;
+
+  refs.text.textContent = `${sum.toFixed(2)}`;
+  refs.amount.textContent = valueQuantity;
+
+  return sum;
+  
+} 
+
+window.addEventListener('DOMContentLoaded', calc);
+
+refs.form.addEventListener('submit', calc);
+
+// 2.2 ПЕРЕПИШІТЬ КОД , ЩОБ ЗНАЧЕННЯ МИ ОТРИМУВАЛИ ПО КЛІЦІ НА КНОПКУ , ПРИ НАЖАТТІ ЯКОГО БУДЕ ВАМ РАХУВАТИ ТА ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН    <button class="btn">Calculate</button>
+
+function quantity () {
+  const valueQuantity = refs.quantity.value;
+  refs.amount.textContent = valueQuantity;
+}
+
+refs.quantity.addEventListener('input', quantity);
